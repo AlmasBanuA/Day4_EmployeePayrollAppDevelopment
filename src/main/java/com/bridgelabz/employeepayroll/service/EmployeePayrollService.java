b317052dc26bdcb76dcc8db9e1fd2ef7d85eb2e7
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-04-22 15:52:35 +0530

    ADDED: employee payroll having different Http methods

---
+++ src/main/java/com/bridgelabz/employeepayroll/service/EmployeePayrollService.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.employeepayroll.service;
+
+import com.bridgelabz.employeepayroll.model.Employee;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService {
+
+    public String getMessage(String name) {
+        return "Welcome "+name;
+    }
+
+    public String postMessage(Employee employee) {
+        return "Hello "+employee.getFirstName()+""+employee.getLastName()+"!";
+    }
+
+    public String putMessage(String name) {
+        return "How are you, "+name;
+    }
+
+}
+
