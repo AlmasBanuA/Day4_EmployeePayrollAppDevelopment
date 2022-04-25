ae476e944e7d65dd62affed330f0b5852bbd0efc
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-04-25 22:38:14 +0530

    Delete src/main directory

--- src/main/java/com/bridgelabz/employeepayrollservice/exception/EmployeePayrollException.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.employeepayrollservice.exception;
-
-public class EmployeePayrollException extends RuntimeException {
-    public EmployeePayrollException(String message) {
-
-        super(message);
-    }
-}
