00faed45ac54ba67c385080559f6366d4787158c
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-04-25 15:20:53 +0530

    UPDATED: added validations to fields so rest calls can be verified

--- src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,79 +17,65 @@ import java.util.Optional;
  * Spring RestController annotation is used to create RESTful web services using Spring MVC.
  */
 @RestController
+@RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
-    /**
-     * @Autowired annotation is used in setter methods to inject the value of the class properties.
-     * When the bean is loaded in the ApplicationContext, the setter method is automatically called by the
-     * spring boot and the value is assigned.
-     */
+
     @Autowired
     IEmployeePayrollService service;
 
-    /**
-     * Get data - Ability to display welcome message
-     * with the help of ResponseEntity we can send the data with the HttpsStatus
-     * @return -returns the welcome message
-     */
-    @GetMapping("/employeePayrollService")
-    public ResponseEntity<String> getWelcome() {
-        return new ResponseEntity<String>(service.getWelcome(), HttpStatus.OK);
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(@RequestParam String name){
+        String message = service.getMessage(name);
+        return new ResponseEntity<String>(message,HttpStatus.OK);
     }
-
-    /**
-     * create record and Ability to save employee details to repository
-     * @apiNote accepts the employee data in JSON format and stores it in DB
-     * @param employee- represents object of EmployeeDTO class
-     * @return accepted employee information in JSON format
-     */
-    @PostMapping("/employeePayrollService/create")
-    public ResponseEntity<Employee> saveDataToRepo(@RequestBody EmployeeDTO employee) {
-        return new ResponseEntity<Employee>(service.postDataToRepo(employee), HttpStatus.OK);
+    @PostMapping("/postMessage")
+    public ResponseEntity<String> postMessage(@Valid @RequestBody EmployeeDTO employeeDTO){
+        String message = service.postMessage(employeeDTO);
+        return new ResponseEntity<String>(message,HttpStatus.OK);
     }
-
-    /**
-     * Ability to get all employees' data by findAll() method
-     * @return list of employee information from DB in JSON format
-     */
-    @GetMapping("/employeePayrollService/get")
-    public ResponseEntity<List<Employee>> getAllDataFromRepo() {
-        return new ResponseEntity<List<Employee>>(service.getAllData(), HttpStatus.OK);
+    @PutMapping("/putMessage/{name}")
+    public ResponseEntity<String> putMessage(@PathVariable String name){
+        String message = service.putMessage(name);
+        return new ResponseEntity<String>(message,HttpStatus.OK);
     }
-
-    /**
-     * Ability to get employee data by id
-     * @param id - represents employee id
-     * @return employee information with same id in JSON format
-     */
-    @GetMapping("/employeePayrollService/get/{id}")
-    public ResponseEntity<Employee> getDataFromRepoById(@PathVariable Integer id) {
-        Optional<Employee> employee = service.getDataById(id);
-        ResponseDTO dto = new ResponseDTO("Data",employee);
-        return new ResponseEntity(dto, HttpStatus.OK);
+    //Ability to display welcome message
+    @GetMapping("")
+    public ResponseEntity<String> getWelcome(){
+        String welcome = service.getWelcome();
+        return new ResponseEntity<String>(welcome,HttpStatus.OK);
     }
-
-    /**
-     * Ability to update employee data for particular id
-     * @apiNote accepts the employee data in JSON format and updates the employee having same id from database
-     * @param id - represents employee id
-     * @param employeeDTO - represents object of EmployeeDTO class
-     * @return	updated employee information in JSON format
-     */
-    @PutMapping("/employeePayrollService/update/{id}")
-    public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id, @RequestBody EmployeeDTO employeeDTO) {
-        Employee employee = service.updateDataById(id, employeeDTO);
-        ResponseDTO responseDTO = new ResponseDTO("Updating Employee PayrollData Successfuly:", employee);
-        return new ResponseEntity(employeeDTO, HttpStatus.OK);
+    //Ability to save employee data to repo
+    @PostMapping("/create")
+    public ResponseEntity<String> addDataToRepo(@Valid @RequestBody EmployeeDTO employeeDTO){
+        Employee newEmployee = service.postDataToRepo(employeeDTO);
+        ResponseDTO dto = new ResponseDTO("Record Added Succesfully",newEmployee);
+        return new ResponseEntity(dto,HttpStatus.CREATED);
     }
-
-    /**
-     * delete records from database from particular id
-     * @apiNote accepts the id and deletes the data of that employee from DB
-     * @param id - represents employee id
-     * @return id and Acknowledgment message
-     */
-    @DeleteMapping("/employeePayrollService/delete/{id}")
-    public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id) {
-        return new ResponseEntity<String>(service.deleteDataById(id), HttpStatus.OK);
+    //Ability to get all employees' data by findAll() method
+    @GetMapping("/get")
+    public ResponseEntity<String>getAllDataFromRepo(){
+        List<Employee> listOfEmployee = service.getAllData();
+        ResponseDTO dto = new ResponseDTO("Record Retrieved Successfully", listOfEmployee);
+        return new ResponseEntity(dto,HttpStatus.OK);
+    }
+    //Ability to get employee data by id
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getDataFromRepoById(@PathVariable Integer id){
+        Optional<Employee> existingEmployee = service.getDataById(id);
+        ResponseDTO dto = new ResponseDTO("Record for given ID Retrieved Successfully", existingEmployee);
+        return new ResponseEntity(dto,HttpStatus.OK);
+    }
+    //Ability to update employee data for particular id
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id,@Valid @RequestBody EmployeeDTO employeeDTO){
+        Employee updatedEmployee = service.updateDataById(id, employeeDTO);
+        ResponseDTO dto = new ResponseDTO("Record for particular ID Updated Successfully",updatedEmployee);
+        return new ResponseEntity(dto,HttpStatus.ACCEPTED);
+    }
+    //Ability to delete employee data for particular id
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id){
+        ResponseDTO dto = new ResponseDTO("Record for particular ID Deleted Successfully", service.deleteDataById(id));
+        return new ResponseEntity(dto,HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeeDTO.java
@@ -1,38 +1,33 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public class EmployeeDTO {
-    /**
-     * Declaring variables
-     * The access level of a private modifier is only within the class. It cannot be accessed from outside the class.
-     */
+
+    @Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee firstName is Invalid")
     private String firstName;
+
+    @Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee lastName is Invalid")
     private String lastName;
+
+    @NotEmpty(message="ProfilePic cannot be null")
     private String profilePic;
+
+    @NotEmpty(message="Department name cannot be null")
     private String department;
-    private Long salary;
+
+    @Min(value=500,message="Salary should be more than 500")
+    private Long salary ;
     private LocalDate date;
     private String notes;
 
-    /**
-     * The super keyword in Java is a reference variable which is used to refer immediate parent class object.
-     */
     public EmployeeDTO() {
-
         super();
     }
 
-    /**
-     * created parameterized constructor with parameters
-     * @param firstName
-     * @param lastName
-     * @param profilePic
-     * @param department
-     * @param salary
-     * @param date
-     * @param notes
-     */
     public EmployeeDTO(String firstName, String lastName, String profilePic, String department, Long salary,
                        LocalDate date, String notes) {
         super();
@@ -45,48 +40,59 @@ public class EmployeeDTO {
         this.notes = notes;
     }
 
-    /**
-     * created getter setters
-     * @return
-     */
     public String getFirstName() {
-
         return firstName;
     }
 
-    public String getLastName() {
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getProfilePic() {
-
         return profilePic;
     }
 
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
 
     public String getDepartment() {
-
         return department;
     }
 
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
     public Long getSalary() {
-
         return salary;
     }
 
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
 
     public LocalDate getDate() {
-
         return date;
     }
 
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
     public String getNotes() {
-
         return notes;
     }
 
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
