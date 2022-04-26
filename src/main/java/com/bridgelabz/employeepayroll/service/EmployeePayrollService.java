dcbbe64174a789906c636b5694b72dffb51c131e
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-04-26 20:34:32 +0530

    UPDATED: ability to throw user friendly error if id not found

--- src/main/java/com/bridgelabz/employeepayroll/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/service/EmployeePayrollService.java
@@ -1,92 +1,86 @@
-package com.bridgelabz.employeepayroll.service;
+package com.bridgelabz.employeepayrollservice.service;
 
-import com.bridgelabz.employeepayroll.dto.EmployeeDTO;
-import com.bridgelabz.employeepayroll.model.Employee;
-import com.bridgelabz.employeepayroll.repository.EmployeePayrollRepository;
+import com.bridgelabz.employeepayrollservice.dto.EmployeeDTO;
+import com.bridgelabz.employeepayrollservice.exception.EmployeePayrollException;
+import com.bridgelabz.employeepayrollservice.model.Employee;
+import com.bridgelabz.employeepayrollservice.repository.EmployeePayrollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-/**
- * @Service marks a Java class that performs some service,
- * such as executing business logic, performing calculations, and calling external APIs.
- */
+
 @Service
+
+/**
+* Created EmployeePayrollService class to serve api calls done by controller layer
+ */
 public class EmployeePayrollService implements IEmployeePayrollService {
+
+    /**
+    * Autowired EmployeePayrollRepository interface to inject its dependency here
+     */
     @Autowired
     EmployeePayrollRepository repository;
 
-    public String getMessage(String name) {
-        return "Welcome To Employee Program " + name;
-    }
-
-    public String postMessage(EmployeeDTO employee) {
-        return "Hello Employee " + employee.getFirstName() + "" + employee.getLastName() + "!";
-    }
-
-    public String putMessage(String name) {
-        return "Hey Dude , " + name;
-    }
-
     public String getWelcome() {
-        return "Welcome to Employee Payroll App.....!";
+
+        return "Welcome to Employee Payroll !!!";
     }
 
-    /**
-     * accepts the employee data in the form of EmployeeDTO and stores it in DB
-     * @param employeeDTO - represents object of EmployeeDTO class
-     * @return accepted employee information in JSON format
-     */
-    @Override
-    public Employee postDataToRepo(EmployeeDTO employee) {
-        Employee newEmployee = new Employee(employee);
+    public Employee postDataToRepo(EmployeeDTO employeeDTO) {
+        Employee newEmployee = new Employee(employeeDTO);
         repository.save(newEmployee);
         return newEmployee;
     }
 
-    /**
-     * @return list of employee information from DB
-     */
-    @Override
     public List<Employee> getAllData() {
         List<Employee> list = repository.findAll();
         return list;
     }
 
-    /**
-     * @param id - represents employee id
-     * @return employee information with same empId
-     */
-    @Override
-    public Optional<Employee> getDataById(Integer id) {
+    public Employee getDataById(Integer id) {
         Optional<Employee> newEmployee = repository.findById(id);
-        return newEmployee;
+        if (newEmployee.isPresent()) {
+            return newEmployee.get();
+        } else throw new EmployeePayrollException("Employee id not found");
     }
 
-    /**
-     * accepts the employee data in the form of EmployeeDTO and
-     * updates the employee having same id from database
-     * @param id - represents employee id
-     * @param employeeDTO - represents object of EmployeeDTO class
-     * @return	updated employee information in JSON format
-     */
     public Employee updateDataById(Integer id, EmployeeDTO employeeDTO) {
-        Employee newEmployee = new Employee(id, employeeDTO);
-        repository.save(newEmployee);
-        return newEmployee;
+        Optional<Employee> newEmployee = repository.findById(id);
+        if (newEmployee.isPresent()) {
+            Employee employee = new Employee(id, employeeDTO);
+            repository.save(employee);
+            return employee;
+        } else {
+            throw new EmployeePayrollException("Employee Not found");
+        }
+    }
+
+    public String deleteDataById(Integer id) {
+        Optional<Employee> newEmployee = repository.findById(id);
+        if (newEmployee.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new EmployeePayrollException("Employee Details not found");
+        }
+      return null;
     }
 
     /**
-     * accepts the id and deletes the data of that employee from DB
-     * @param id - represents employee id
-     * @return empId and Acknowledgment message
+    * Ability to serve controller class api to retrieve data having particular department
      */
-    public String deleteDataById(Integer id) {
-        repository.deleteById(id);
-        return "Employee with unique ID:" + id + " got deleted";
+    public List<Employee> getDataByDepartment(String department) {
+        List<Employee> newEmp = repository.findByDepartment(department);
+        if (newEmp.isEmpty()) {
+            throw new EmployeePayrollException("Employee Not Found");
+        }
+        return newEmp;
     }
-
-
 }
+
+
+
+
+
