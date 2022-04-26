66370bdf929bc9fb3b7b2749eb97bf9f2c213f4c
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-04-26 20:30:49 +0530

    UPDATED: ability to throw user friendly error if id not found

---
+++ src/main/java/com/bridgelabz/employeepayroll/exception/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollservice.exception;
+
+public class EmployeePayrollException extends RuntimeException {
+    public EmployeePayrollException(String message) {
+
+        super(message);
+    }
+}
