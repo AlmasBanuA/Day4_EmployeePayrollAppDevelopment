2c50fddc6c38f46fa40d000d469d671fa64a248a
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-04-26 20:28:37 +0530

    UPDATED: ability to throw user friendly error if id not found

---
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeeDTO.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.employeepayrollservice.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created EmployeeDTO class and added validations to fields
+ * @Data :-
+ *        Lombok Data annotation ( @Data ) Generates getters for all fields, a useful toString method,
+ *        and hashCode and equals implementations that check all non-transient fields.
+ *        Will also generate setters for all non-final fields, as well as a constructor.
+ */
+@Data
+public class EmployeeDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee firstName is Invalid")
+    private String firstName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee lastName is Invalid")
+    private String lastName;
+
+    @NotEmpty(message = "ProfilePic cannot be null")
+    private String profilePic;
+
+    @NotEmpty(message = "Department name cannot be null")
+    private List<String> department;
+
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
+    private String gender;
+
+    @Min(value = 500, message = "Salary should be more than 500")
+    private Long salary;
+
+    @PastOrPresent(message = "Date should be past or today date")
+    private LocalDate date;
+    private String notes;
+
+    public EmployeeDTO() {
+
+        super();
+    }
+}
